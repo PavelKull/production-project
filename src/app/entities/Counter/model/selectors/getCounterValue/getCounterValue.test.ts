4499077417bbf8a86723fa2d456aa090f247fea1
PavelK <pavelkulikov-s@mail.ru>
2024-01-14 17:40:08 +0300

    update profile, private routes, ui select, country select

--- src/app/entities/Counter/model/selectors/getCounterValue/getCounterValue.test.ts
+++ src/app/entities/Counter/model/selectors/getCounterValue/getCounterValue.test.ts
@@ -1,4 +1,3 @@
-import { DeepPartial } from '@reduxjs/toolkit';
 import { StateSchema } from 'app/providers/StoreProvider';
 import { CounterSchema } from '../../types/counterSchema';
 import { getCounterValue } from './getCounterValue';
